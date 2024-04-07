ab9238d9611fe1f3e7c6304135d787c7d45ec0e8
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 16:26:50 +0530

    Update page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -6,14 +6,17 @@ import { search } from '../functions/gogo.js';
 
 function Result() {
   const [data, setData] = useState({ results: [] });
+  const [loading, setLoading] = useState(false);
   const params = useSearchParams();
   const anime = params.get('anime');
 
   useEffect(() => {
     if (anime) {
+      setLoading(true);
       search(anime).then((ans) => {
         console.log(ans);
         setData(ans);
+        setLoading(false);
       });
     }
   }, [anime]);
@@ -21,7 +24,9 @@ function Result() {
   const m = data.results;
   return (
     <div className="mt-10 mr-6 ml-6">
-    {m.length > 0 ? (m?.map((x, index) => ( 
+    {loading ? (
+        <p>Loading...</p> // Display loading message while fetching data
+      ) : m.length > 0 ? (m?.map((x, index) => ( 
     <div key={index} className="border-2"> 
     <span className="text-base">Anime: {x.title}</span>
      </div>
