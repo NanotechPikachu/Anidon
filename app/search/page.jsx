0b1c2e87d34432046af7808370786d37ae8ca892
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 14:05:28 +0530

    Update page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -1,5 +1,19 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
 export default function Result() {
+  const [data, setData] = useState(null);
+  const [query, setQuery] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.search) {
+      setQuery(router.query.search)
+    }
+  }, [router.query]);
   return (
-    <div>Hi</div>
+    <div>{query}</div>
   )
 }
