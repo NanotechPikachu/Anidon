5ae68bbcd42f8f79897c469ac5ac362c5f329b18
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 15:34:23 +0530

    Update page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -6,7 +6,7 @@ import { search } from '../functions/gogo.js';
 
 function Result() {
   const [data, setData] = useState('Waiting...');
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState(null);
   const params = useSearchParams();
   const anime = params.get('anime');
 
@@ -14,13 +14,18 @@ function Result() {
     if (anime) {
       setQuery(anime)
       const ans = search(query).then((ans) => {
-      setData(ans)
+      setData(ans?.results)
     });
     }
   }, [anime]);
 
   return (
-    <>{JSON.stringify(data)}</>
+    <>
+    <div className="mt-10 mr-6 ml-6">
+    <h1 className="font-bold">{data}
+    </h1>
+    </div>
+    </>
   )
 }
 export default function Res() {
