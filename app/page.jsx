fb40c4eb369e8b2e2dec2198a2dbe36a5bb88337
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 13:51:33 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState('');
