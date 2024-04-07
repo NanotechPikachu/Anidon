ab7b501fa8ef7dabaa31c24954958a87f7feaca0
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 18:23:43 +0530

    Update page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from 'next/navigation';
 import { useState, useEffect, Suspense } from 'react';
 import { search } from '../functions/gogo.js';
+import Image from 'next/image'
 
 function Result() {
   const [data, setData] = useState({ results: [] });
@@ -23,21 +24,29 @@ function Result() {
 
   const m = data.results;
   return (
-    <div className="mt-10 mr-6 ml-6">
+    <>
+    <div className="ml-6 mr-6">
+    <div className="mt-18">
+    <h1 className="text-xl text-center font-bold">Search results...</h1>
+    </div>
+    <div className="mt-6 grid grid-flow-row grid-cols-3 overflow-hidden gap-4"> 
     {loading ? (
-        <p>Loading...</p> // Display loading message while fetching data
+        <p>Loading...</p>
       ) : m.length > 0 ? (m?.map((x, index) => ( 
-    <div key={index} className="border-2"> 
-    <span className="text-base">Anime: {x.title}</span>
+    <div key={index} className="border-2 border-teal-100 overflow-hidden h-20 flex flex-col relative">
+    <Image src={x.image} layout="fill" objectFit="cover" alt="Anime pic" />
+    <p className="text-base">Anime: {x.title}</p>
      </div>
     ))) : (<p>No results</p>)}
     </div>
+    </div>
+    </>
   );
 }
 
 export default function Res() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="w-full min-h-screen items-center justify-center><p>Loading...</p></div>}>
       <Result />
     </Suspense>
   );
