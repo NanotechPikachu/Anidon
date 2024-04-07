e0ec8d8b89adb0ea9adbff137e6bfd634eb63a50
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 13:57:06 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -6,9 +6,15 @@ import { useRouter } from 'next/navigation';
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState('');
   const router = useRouter();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    router.push(`search?query=${encodeURIComponent(searchTerm)}`);
+  };
+  
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit}>
     <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for an anime.." />
     <button type="submit">Search</button>
     </form>
