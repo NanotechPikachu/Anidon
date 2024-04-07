89b4a6b88d7cbb16bde33724433d557d4ae21dd9
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 13:35:16 +0530

    fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Anidon",
   description: "Anime web",
 };
