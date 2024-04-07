bc3f3fcce17fef8cfeeaac0bbc99573633d6713e
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-07 13:40:01 +0530

    Update tailwind.config.js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,14 @@
-import type { Config } from "tailwindcss";
+/** {import('tailwindcss').Config} */
 
-const config: Config = {
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'media',
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
-export default config;
