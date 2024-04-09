8baf921ab018009e623b17168fd3251298f71a22
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 13:04:42 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'gogocdn.net',
+        pathname: '/images/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
