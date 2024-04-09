019f403beb5062645dec75407c94e1765b3aff63
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 16:01:57 +0530

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,16 +1,16 @@
-import { useRouter } from 'next/navigation';
+//import { useRouter } from 'next/navigation';
  
 export default function NotFound() {
   const router = useRouter();
  
-  function change() {
+ /* function change() {
     router.push('/');
-  };
+  };*/
  
   return (
     <div className="flex flex-grow justify-center w-full min-h-screen items-center flex-col">
     <h2 className="text-lg md:text-2xl xl:text-3xl">Seems like you&apos;re lost</h2>
-    <button className="border-2 border-violet-700 text-indigo-400 font-bold p-3 rounded-full" onClick={() => change()}>Return Home</button>
+    <button className="border-2 border-violet-700 text-indigo-400 font-bold p-3 rounded-full" onClick={/*() => change()*/}>Return Home</button>
     </div>
   )
 }
