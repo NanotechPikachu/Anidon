1a40cf009e9ed7bf699ff5b8f341540294d15eab
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 14:10:22 +0530

    Create loading.jsx

---
+++ app/loading.jsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <>
+    <div className="flex flex-grow w-full min-h-screen justify-center items-center">
+    <p className="text-lg md:text-xl text-violet-600">Loading...</p>
+    </div>
+    </>
+  )
+}
