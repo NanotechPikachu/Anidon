deb95e70d0ecfa4257bb9ea4b349167f041113ad
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 13:50:26 +0530

    Update page.jsx

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -43,12 +43,11 @@ function Anime() {
     <div>
     <div className="mt-20">
     <h1 className="text-2xl text-center font-bold">{`${m.title?.replace('(Dub)', '')} (${m.subOrDub})`}</h1>
-<Image src={m.image} />
     </div>
     <div className="mt-4 flex justify-center items-center w-full bg-cover h-[252px] bg-no-repeat bg-center" style={{ backgroundImage: `url('${m.image}')`, height: "100%", width: "100%" }}>
     <div className="h-full w-full backdrop-blur-sm">
     <div className="h-48 w-44">
-    <img src={m.image} alt="Anime pic" style={{ width: '224px', height: '250px', objectFit: 'contain', float: 'left' }} />
+    <Image src={m.image} alt="Anime pic" width={224} height={250} objectFit="contain" style={{ float: 'left' }} />
     </div>
     </div>
     </div>
