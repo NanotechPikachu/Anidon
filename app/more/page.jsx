b10144322f400cb9c50045e251464d7523b9e3df
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 21:44:08 +0530

    Update page.jsx

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -1,7 +1,16 @@
 import { Suspense } from 'react';
+import { trending } from '../functions/anilist.js';
 
 async function More() {
-  const m = false;
+  const data = await trending();
+
+  if (data.length <= 0) {
+    return (
+      <div className="flex flex-grow w-full min-h-screen items-center justify-center text-center"><p className="text-orange-500 dark:text-orange-400 text-lg md:text-xl">Something went wrong when fetching the information!</p></div>
+    )
+  };
+
+  m = data;
 
   return (
     <>
@@ -9,8 +18,12 @@ async function More() {
     {m ? (
     <div>
     <div className="mt-20">
-    <h1 className="text-2xl text-center font-bold text-violet-900 hover:text-purple-400">{anime}</h1>
-    <p className="font-mono mt-3 text-sm md:text-base text-center text-slate-50">{`Episode: ${episode}`}</p>
+    <h1 className="text-2xl text-center font-bold text-violet-900 hover:text-purple-400">Trending</h1>
+    </div>
+    <div className="grid grid-flow-col max-w-screen grid-rows-1 gap-2 overflow-x-auto">
+    {m.map((x, index) => (
+    <div className="h-28 w-20" key={x?.title?.romaji}><img src={x.image} /></div>
+    ))}
     </div>
     </div>
     ) : (<div className="flex flex-grow w-full min-h-screen items-center justify-center text-center"><p className="text-orange-500 dark:text-orange-400 text-lg md:text-xl">No results Found!</p></div>)}
