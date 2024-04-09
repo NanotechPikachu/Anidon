be04333fed2222491c8516a539dbeeee1dba8cca
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 18:33:07 +0530

    Removing the Test <div />

--- app/page.jsx
+++ app/page.jsx
@@ -18,11 +18,6 @@ export default function Home() {
     <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for an anime.." />
     <button type="submit">Search</button>
     </form>
-<div className="group relative bg-black text-white p-4 overflow-hidden">
-  <div className="absolute inset-0 bg-violet-500 transition-all duration-500 ease-in-out group-hover:translate-x-0 translate-x-full"></div>
-  <p className="relative z-10 transition-colors duration-500 ease-in-out group-hover:text-black">Hover over this div</p>
-</div>
-
     </>
   )
 }
