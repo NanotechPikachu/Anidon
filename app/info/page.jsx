3614c7ea7568900bea1599ec36a8c5754e7aed5d
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 12:29:16 +0530

    Update page.jsx

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -37,7 +37,7 @@ function Anime() {
     <>
     <div className="ml-6 mr-6 mt-6 mb-20">
     {loading ? (
-        <div className="flex flex-grow w-full min-h-screen items-center justify-center text-center text-lg md:text-xl text-amber-400 dark:text-amber-300"><p>Loading...</p></div>
+        <div className="flex flex-grow w-full min-h-screen items-center justify-center text-center text-lg md:text-xl text-violet-600"><p>Loading...</p></div>
       ) : m.title ? (
     <div>
     <div className="mt-20">
@@ -63,7 +63,7 @@ function Anime() {
     <h2 className="text-lg mt-8 underline font-bold">Other Name(s)</h2>
     <p className="text-sm">{m.otherName || 'No information'}</p>
     <h2 className="text-lg mt-8 underline font-bold">{`Episode(s) (${m.totalEpisodes})`}</h2>
-    <div className="mt-2 flex flex-col md:grid md:grid-flow-row md:grid-cols-3 overflow-hidden md:gap-4"> {l?.map((x, index) => (<div key={index} onClick={() => changePage(n[index], m.title, index + 1)}><p className="border-2 p-2 cursor-pointer">Episode: {x} </p></div>)) || 'No information'}</div>
+    <div className="mt-2 flex flex-col md:grid md:grid-flow-row md:grid-cols-3 overflow-hidden md:gap-4"> {l?.map((x, index) => (<div key={index} onClick={() => changePage(n[index], m.title, index + 1)}><p className="border-2 border-violet-700 p-2 cursor-pointer">Episode: {x} </p></div>)) || 'No information'}</div>
     </div>
     </div>
     ) : (<div className="flex flex-grow w-full min-h-screen items-center justify-center text-center"><p className="text-orange-500 dark:text-orange-400 text-lg md:text-xl">No results Found!</p></div>)}
@@ -74,7 +74,7 @@ function Anime() {
 
 export default function Info() {
   return (
-    <Suspense fallback={<div className="flex flex-grow w-full min-h-screen items-center justify-center text-center text-lg md:text-xl dark:text-amber-300 text-amber-400"><p>Loading...</p></div>}>
+    <Suspense fallback={<div className="flex flex-grow w-full min-h-screen items-center justify-center text-center text-lg md:text-xl dark:text-violet-600"><p>Loading...</p></div>}>
       <Anime />
     </Suspense>
   );
