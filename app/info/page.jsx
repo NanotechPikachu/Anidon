2c988463734d8bbf9e9fdb07d57bbb04c457efcd
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 14:15:16 +0530

    Update page.jsx

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -42,7 +42,7 @@ function Anime() {
       ) : m.title ? (
     <div>
     <div className="mt-20">
-    <h1 className="text-2xl text-center font-bold">{`${m.title?.replace('(Dub)', '')} (${m.subOrDub})`}</h1>
+    <h1 className="text-2xl text-center font-bold text-white">{`${m.title?.replace('(Dub)', '')} (${m.subOrDub})`}</h1>
     </div>
     <div className="mt-4 flex justify-center items-center w-full bg-cover h-[252px] bg-no-repeat bg-center" style={{ backgroundImage: `url('${m.image}')`, height: "100%", width: "100%" }}>
     <div className="h-full w-full backdrop-blur-sm">
@@ -55,18 +55,18 @@ function Anime() {
     </div>
     <br /> <br /> <br />
     <div className="w-full">
-    <h2 className="text-lg underline font-bold">Description</h2>
-    <p className="text-sm">{m.description || 'No information'}</p>
-    <h2 className="text-lg mt-8 underline font-bold">Status</h2>
-    <p className="text-sm">{m.status || 'No information'}</p>
-    <h2 className="text-lg mt-8 underline font-bold">Genres</h2>
-    <p className="text-sm">{m.genres?.join(", ") || 'No information'}</p>
-    <h2 className="text-lg mt-8 underline font-bold">Release</h2>
-    <p className="text-sm">{m.releaseDate || 'No information'}</p>
-    <h2 className="text-lg mt-8 underline font-bold">Other Name(s)</h2>
-    <p className="text-sm">{m.otherName || 'No information'}</p>
-    <h2 className="text-lg mt-8 underline font-bold">{`Episode(s) (${m.totalEpisodes})`}</h2>
-    <div className="mt-2 flex flex-col md:grid md:grid-flow-row md:grid-cols-3 overflow-hidden md:gap-4"> {l?.map((x, index) => (<div key={index} onClick={() => changePage(n[index], m.title, index + 1)}><p className="border-2 border-violet-700 p-2 cursor-pointer">Episode: {x} </p></div>)) || 'No information'}</div>
+    <h2 className="text-lg underline font-bold text-white">Description</h2>
+    <p className="text-sm text-slate-50">{m.description || 'No information'}</p>
+    <h2 className="text-lg mt-8 underline font-bold text-white">Status</h2>
+    <p className="text-sm text-slate-50">{m.status || 'No information'}</p>
+    <h2 className="text-lg mt-8 underline font-bold text-white">Genres</h2>
+    <p className="text-sm text-slate-50">{m.genres?.join(", ") || 'No information'}</p>
+    <h2 className="text-lg mt-8 underline font-bold text-white">Release</h2>
+    <p className="text-sm text-slate-50">{m.releaseDate || 'No information'}</p>
+    <h2 className="text-lg mt-8 underline font-bold text-white">Other Name(s)</h2>
+    <p className="text-sm text-slate-50">{m.otherName || 'No information'}</p>
+    <h2 className="text-lg mt-8 underline font-bold text-white">{`Episode(s) (${m.totalEpisodes})`}</h2>
+    <div className="mt-2 flex flex-col md:grid md:grid-flow-row md:grid-cols-3 overflow-hidden md:gap-4"> {l?.map((x, index) => (<div key={index} onClick={() => changePage(n[index], m.title, index + 1)}><p className="border-2 border-violet-700 p-2 cursor-pointer text-slate-50">Episode: {x} </p></div>)) || 'No information'}</div>
     </div>
     </div>
     ) : (<div className="flex flex-grow w-full min-h-screen items-center justify-center text-center"><p className="text-orange-500 dark:text-orange-400 text-lg md:text-xl">No results Found!</p></div>)}
