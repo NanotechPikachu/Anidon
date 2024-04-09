12e54877531fdeed2a949bcd99f8ef382e495458
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-09 14:36:39 +0530

    Update error.js

--- app/error.js
+++ app/error.js
@@ -17,18 +17,18 @@ export default function Error({ error, reset }) {
   return (
     <>
     <div className="flex flex-grow min-h-screen w-full justify-center items-center flex-col">
-    <h2 className="text-lg md:text-xl text-orange-600 font-bold">Uh Oh!</h2>
-    <p className="text-base md:text-lg text-orange-600 mt-2">Uh Oh!</p>
-    <div className="flex flex-row w-full justify-center items-center gap-2">
+    <h2 className="text-lg md:text-xl xl:text-2xl text-orange-600 font-bold">Uh Oh!</h2>
+    <p className="text-base md:text-lg xl:text-xl text-orange-600 mt-2">Something went haywire!</p>
+    <div className="flex flex-row w-full justify-center items-center gap-2 mt-6">
     <button
       onClick={() => reset()}
-      className="border-2 border-violet-700"
+      className="border-2 border-violet-700 font-bold p-3 rounded-full text-indigo-400"
     >
     Refresh
     </button>
     <button
       onClick={() => page()}
-      className="border-2 border-violet-700"
+      className="border-2 border-violet-700 font-bold p-3 rounded-full text-indigo-400"
     >
     Go to Home
     </button>
