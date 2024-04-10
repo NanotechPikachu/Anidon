093ad7b9ccc3af8defbda0b0e01ee8405ffa3db8
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-10 08:31:20 +0530

    Update page.jsx

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -21,10 +21,10 @@ async function More() {
     <div className="mt-20">
     <h1 className="text-2xl text-center font-bold text-violet-900 hover:text-purple-400">Trending</h1>
     </div>
-    <div className="mt-4 grid grid-flow-col max-w-screen grid-rows-1 gap-2 overflow-x-auto">
+    <div className="mt-4 grid grid-flow-col max-w-screen grid-rows-1 gap-2 overflow-x-auto h-36">
     {m.map((x, index) => (
-    <div className="h-36 w-32" key={x?.title?.romaji}>
-    <Image src={x.image} alt="Anime pic" objectFit="contain" />
+    <div className="w-32" key={x?.title?.romaji}>
+    <Image src={x.image} alt="Anime pic" objectFit="cover" />
     </div>
     ))}
     </div>
