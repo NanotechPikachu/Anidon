de37e80f337379e0932dd46fceeff697ccca870d
Maghish <mail4mithran@gmail.com>
2024-04-10 09:13:15 +0530

    Redesigned search bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,30 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { FaAngleRight } from "react-icons/fa"
 
 export default function Home() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const router = useRouter();
 
   function handleSubmit(e) {
     e.preventDefault();
     router.push(`search?anime=${encodeURIComponent(searchTerm)}`);
-  };
-  
+  }
+
   return (
-    <>
-    <form onSubmit={handleSubmit}>
-    <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search for an anime.." />
-    <button type="submit">Search</button>
-    </form>
-    </>
-  )
+    <div className="min-w-full min-h-screen flex justify-center p-16">
+      <form className="mt-[80px] flex flex-row gap-x-6 min-h-14 max-h-14" onSubmit={handleSubmit}>
+        <input
+          className="w-[450px] px-4 py-3 rounded-lg outline-none"
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for an anime.."
+        />
+        <button className="w-[120px] rounded-lg bg-blue-500 text-white flex flex-row px-4 py-3 items-center" type="submit">Search <FaAngleRight className="ml-6" size="20px" color="#FFFFFF" /></button>
+      </form>
+    </div>
+  );
 }
