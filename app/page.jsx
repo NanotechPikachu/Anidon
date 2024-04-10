9a655402fb2f0e80fb1d83a3057dfd429302c20c
Maghish <mail4mithran@gmail.com>
2024-04-10 12:34:07 +0530

    Tried to make it mobile responsive

--- app/page.jsx
+++ app/page.jsx
@@ -17,23 +17,28 @@ export default function Home() {
 
   return (
     <div className="min-w-full min-h-screen flex justify-center p-16">
-      <Image src={SoloLevelingBG} objectFit="cover" className="absolute z-0 top-0 left-0 object-cover max-h-screen h-screen min-w-full max-w-full opacity-30"></Image>
+      <Image
+        src={SoloLevelingBG}
+        objectFit="cover"
+        className="absolute z-0 top-0 left-0 object-cover max-h-screen h-screen min-w-full max-w-full opacity-20"
+      ></Image>
       <form
-        className="mt-[80px] flex flex-row gap-x-6 min-h-14 max-h-14 z-10"
+        className="mx-[10px] mt-[80px] flex flex-row gap-x-6 h-auto max-h-14 z-10"
         onSubmit={handleSubmit}
       >
         <input
-          className="w-auto md:w-[450px] px-4 py-3 rounded-lg outline-none text-sm"
+          className="w-fit sm:w-auto md:w-[450px] px-2 sm:px-4 py-2 sm:py-3 rounded-lg outline-none text-sm"
           type="text"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for an anime.."
         />
         <button
-          className="w-[120px] rounded-lg bg-blue-500 text-white flex flex-row px-4 py-3 items-center"
+          className="text-sm sm:text-base w-fit sm:w-[120px] rounded-lg bg-violet-500 text-white flex flex-row px-2 sm:px-4 py-2 sm:py-3 items-center"
           type="submit"
         >
-          Search <FaAngleRight className="ml-6" size="20px" color="#FFFFFF" />
+          Search{" "}
+          <FaAngleRight className="ml-3 sm:ml-6" size="20px" color="#FFFFFF" />
         </button>
       </form>
     </div>
