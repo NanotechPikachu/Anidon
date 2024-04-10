dc6746c67a914523ea2c5171f250b29e62daef06
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-10 15:59:55 +0530

    Update NavBar.jsx

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -31,11 +31,11 @@ export default function NavBar({ current }) {
 
   return (
     <div>
-    <div className="backdrop-blur fixed top-0 left-0 w-full flex items-center justify-between py-4 px-8 border-b border-primary-color shadow-md z-20">
+    <div className="backdrop-blur fixed top-0 left-0 w-full flex items-center justify-between py-4 md:py-8 px-8 border-b border-primary-color shadow-md z-20">
     <p className="font-bold text-left text-violet-800 hover:text-purple-400 text-xl font-sans">
-    Nanotech Wiki
+    Anidon
     </p>
-    <div className={`fixed top-0 right-0 m-3 p-1 border-2 hover:border-sky-400 ${isOpen ? 'hidden' : 'block'}`}
+    <div className={`fixed top-0 right-0 m-3 p-1 border-2 border-violet-900 hover:border-purple-500 ${isOpen ? 'hidden' : 'block'}`}
         onClick={toggleNavBar}>
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
@@ -47,11 +47,16 @@ export default function NavBar({ current }) {
           isOpen ? 'translate-x-0' : 'translate-x-full'
         } transition-transform duration-500 ease-in-out`}
       >
-    <div className="absolute top-0 left-0 m-3 p-1 border-2 hover:border-sky-400"
+    <div className="absolute top-0 left-0 m-3 p-1 border-2 border-violet-900 hover:border-purple-500"
           onClick={toggleNavBar}>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4">
+    <path fillRule="evenodd" d="M14 8a.75.75 0 0 1-.75.75H4.56l3.22 3.22a.75.75 0 1 1-1.06 1.06l-4.5-4.5a.75.75 0 0 1 0-1.06l4.5-4.5a.75.75 0 0 1 1.06 1.06L4.56 7.25h8.69A.75.75 0 0 1 14 8Z" clipRule="evenodd" />
+    </svg>
+{/*
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9" />
     </svg>
+*/}
     </div>
     <NavLinks toggleNavBar={toggleNavBar} />
     </div>
