57088c4a5b8d642a5974e1ce0f231e9eca53a47c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-10 12:28:43 +0530

    Update page.jsx

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -1,30 +1,32 @@
 import { Suspense } from 'react';
-import { trending } from '../functions/anilist.js';
+import { trending, popular } from '../functions/anilist.js';
 import Image from 'next/image';
 
 async function More() {
   const data = await trending();
+  const dat = await popular();
 
-  if (data.length <= 0) {
+  if (data.length <= 0 || dat.length <= 0) {
     return (
       <div className="flex flex-grow w-full min-h-screen items-center justify-center text-center"><p className="text-orange-500 dark:text-orange-400 text-lg md:text-xl">Something went wrong when fetching the information!</p></div>
     )
   };
 
   const m = data;
+  const n = dat;
 
   return (
     <>
     <div className="ml-6 mr-6 mt-6 mb-20">
     {m ? (
-    <div className="border-violet-700 border-2 mt-20 bg-violet-600">
+    <div className="mt-20">
     <div>
-    <h1 className="text-2xl text-center font-bold bg-opacity-50 backdrop-blur-sm bg-clip-text bg-gradient-to-r text-orange-400 from-orange-400 via-orange-300/25 to-orange-400 animate-gloss">Trending</h1>
+    <h1 className="text-2xl text-center font-bold bg-opacity-50 backdrop-blur-sm bg-clip-text bg-gradient-to-r text-white from-slate-50 via-white/25 to-slate-50 animate-gloss">Trending</h1>
     </div>
     <div className="mt-4 mb-4 grid grid-flow-col auto-cols-max max-w-screen grid-rows-1 gap-0.5 overflow-x-auto h-36">
     {m.map((x, index) => (
-    <div className="relative w-32 h-full flex items-stretch" key={x?.title?.romaji}>
-    <Image src={x.image} alt="Anime pic" className="w-full h-full object-contain" />
+    <div className="border-2 border-violet-500 relative w-32 h-full flex items-stretch" key={x?.title?.romaji}>
+    <Image src={x.image} alt="Anime pic" className="w-full h-full object-contain opacity-50" />
     </div>
     ))}
     </div>
