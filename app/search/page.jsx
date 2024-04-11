d7db3876a821776b0eb39d3b380e62cea708012a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-11 13:33:00 +0530

    Update page.jsx

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -36,16 +36,16 @@ function Result() {
     <h1 className="md:text-3xl text-2xl text-center font-bold text-white">Search results...</h1>
     <p className="text-center font-mono text-sm md:text-base text-slate-50"><i>Query: {anime}</i></p>
     </div>
-    <div className="mt-6 grid grid-flow-row grid-cols-3 md:grid-cols-5 overflow-hidden gap-4 justify-center justify-items-center items-center"> 
+    <div className="mt-6 grid grid-flow-row grid-flow-col overflow-hidden gap-4 justify-center justify-items-center items-center"> 
     {loading ? (
     <p className="text-violet-600 text-lg md:text-xl animate-bounce">Loading...</p>
       ) : m.length > 0 ? (m?.map((x, index) => ( 
-    <div key={index} className="border-2 border-violet-700 overflow-hidden flex flex-col cursor-pointer w-24 md:w-36 h-44 md:h-64 hide-scroll relative" onClick={() => changePage(x.id)}>
+    <div key={index} className="overflow-hidden flex flex-col cursor-pointer w-24 md:w-36 h-44 md:h-64 hide-scroll relative" onClick={() => changePage(x.id)}>
     <div className="absolute z-10 left-0.5 top-0.5 text-black bg-violet-500 text-xs rounded-md p-0.5">{x.subOrDub}</div>
     <div className="w-24 md:w-36 h-36 md:h-56">
     <img src={x.image} alt="Anime pic" className="w-full h-full opacity-75 object-cover"></img>
     </div>
-    <p className="text-sm md:text-base text-white text-center overflow-hidden truncate">{x.title}</p>
+    <p className="text-xs md:text-sm md:text-base text-white text-center overflow-hidden truncate">{x.title}</p>
      </div>
     ))) : (<p className="text-orange-500 dark:text-orange-400 text-lg md:text-xl">No results</p>)}
     </div>
