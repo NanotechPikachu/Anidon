f7cab25db695b6a208613c30547ff56b62f5ffed
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-14 15:14:22 +0530

    PopularAnime graphq

--- app/functions/anilist.js
+++ app/functions/anilist.js
@@ -22,8 +22,20 @@ async function trending() {
 };
 
 async function popular() {
-  const result = await anilist.fetchPopularAnime();
-  return result?.results;
+  const p = path.resolve(process.cwd(), './app/functions/popularAnime.graphql');
+  const query = fs.readFileSync(p, 'utf-8');
+  const d = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      query: query
+    })
+  }).then(res => res.json()).catch(err => console.error(err));
+  console.log(d)
+  return d.data.Page.media;
 };
 
 export { trending, popular }
