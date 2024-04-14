c479ed5f09cefeb329f02f9ac332a9f876d7646a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-14 19:13:55 +0530

    Update page.jsx

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -25,6 +25,27 @@ async function More() {
 
   return (
     <>
+      <div className="min-w-full min-h-screen flex justify-center p-16">
+      <form
+        className="mx-[10px] mt-[80px] flex flex-row gap-x-6 h-auto max-h-14 z-10"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="w-fit sm:w-auto md:w-[450px] px-2 sm:px-4 py-2 sm:py-3 rounded-lg outline-none text-sm"
+          type="text"
+          value={""/*searchTerm*/}
+          onChange={console.log("hi")/*(e) => setSearchTerm(e.target.value)*/}
+          placeholder="Search for an anime.."
+        />
+        <button
+          className="text-sm sm:text-base w-fit sm:w-[120px] rounded-lg bg-violet-500 text-white flex flex-row px-2 sm:px-4 py-2 sm:py-3 items-center"
+          type="submit"
+        >
+          Search{" "}
+          <FaAngleRight className="ml-3 sm:ml-6" size="20px" color="#FFFFFF" />
+        </button>
+      </form>
+      </div>
       <div className="ml-6 mr-6 mt-6 mb-20">
         {m ? (
           <>
