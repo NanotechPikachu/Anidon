9438467925fec253193187aea317062668766fb3
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-14 14:56:50 +0530

    Update anilist.js

--- app/functions/anilist.js
+++ app/functions/anilist.js
@@ -1,5 +1,4 @@
 import { META } from '@consumet/extensions';
-//const { requireText } = './require-text.js';
 import fs from 'node:fs';
 import path from 'node:path';
 const url = 'https://graphql.anilist.co';
@@ -7,9 +6,7 @@ const url = 'https://graphql.anilist.co';
 const anilist = new META.Anilist();
 
 async function trending() {
-  //const query = requireText('./trendingAnime.graphql', require);
   const p = path.resolve(process.cwd(), './app/functions/trendingAnime.graphql');
-  console.log(p)
   const query = fs.readFileSync(p, 'utf-8');
   const d = await fetch(url, {
     method: 'POST',
@@ -21,7 +18,6 @@ async function trending() {
       query: query
     })
   }).then(res => res.json()).catch(err => console.error(err));
-  console.log(JSON.stringify(d));
   return d.data.Page.media;
 };
 
