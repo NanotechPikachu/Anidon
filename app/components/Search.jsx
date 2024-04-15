1023c222999af4e35e495cccf930da1063160959
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-15 10:52:21 +0530

    Update Search.jsx

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -1,14 +1,19 @@
 "use client";
 
-import { FaAngleRight } from "react-icons/fa";
+import { useState } from 'react';
+import { FaAngleRight } from 'react-icons/fa';
 
 export default function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   function handleSubmit(e) {
     e.preventDefault();
+    // Implement what should happen when the form is submitted
+    console.log(`Search for: ${searchTerm}`);
   }
 
   return (
-      <div className="min-w-full min-h-screen flex justify-center p-16">
+    <div className="min-w-full min-h-screen flex justify-center p-16">
       <form
         className="mx-[10px] mt-[80px] flex flex-row gap-x-6 h-auto max-h-14 z-10"
         onSubmit={handleSubmit}
@@ -16,8 +21,8 @@ export default function Search() {
         <input
           className="w-fit sm:w-auto md:w-[450px] px-2 sm:px-4 py-2 sm:py-3 rounded-lg outline-none text-sm"
           type="text"
-          value={""/*searchTerm*/}
-          onChange={console.log("hi")/*(e) => setSearchTerm(e.target.value)*/}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           placeholder="Search for an anime.."
         />
         <button
@@ -28,6 +33,6 @@ export default function Search() {
           <FaAngleRight className="ml-3 sm:ml-6" size="20px" color="#FFFFFF" />
         </button>
       </form>
-      </div>
-  )
+    </div>
+  );
 }
