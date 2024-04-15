c719c10f577bf61dfc4848963964b447b45558a4
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-15 10:38:58 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Replace 'fs' module with an empty object on the client side to prevent errors
+      config.resolve.fallback = { fs: false };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
