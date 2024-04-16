da54c52f7f28045b5fc7a0046b4a21c0955f1221
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-16 15:54:34 +0530

    Update Refresh.jsx

--- app/functions/Refresh.jsx
+++ app/functions/Refresh.jsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 export function Refresh() {
   const router = useRouter();
   useEffect(() => {
-    const timeout = setTimeout(() => router.refresh(), 60);
+    const timeout = setTimeout(() => { router.refresh(); console.log("+")}, 60);
     return () => clearTimeout(timeout);
   }, []);
   return null;
