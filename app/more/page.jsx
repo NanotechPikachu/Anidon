49fa0d7f3085d4e382e47e5ce8f9c362b6cfdca5
Maghish <mail4mithran@gmail.com>
2024-04-16 12:08:39 +0530

    Redesigned Popular Anime image cards

--- app/more/page.jsx
+++ app/more/page.jsx
@@ -37,10 +37,10 @@ async function More() {
                 TRENDING ANIME
               </p>
             </div>
-            <div className="hover:small-scroll mt-6 mb-4 grid grid-flow-col auto-cols-max max-w-screen grid-rows-1 gap-3 md:gap-6 overflow-x-auto h-44 md:h-72">
+            <div className="hover:small-scroll mt-6 mb-4 grid grid-flow-col auto-cols-max max-w-screen grid-rows-1 gap-3 md:gap-6 overflow-x-auto h-72">
               {m.map((x, index) => (
                 <div
-                  className="overflow-hidden flex flex-col flex-shrink-0 cursor-pointer sm:w-44 sm:h-72 h-60 w-36 hide-scroll relative rounded-md"
+                  className="overflow-hidden flex flex-col flex-shrink-0 cursor-pointer sm:w-44 h-72 w-36 hide-scroll relative rounded-md"
                   key={x?.title?.userPreferred}
                 >
                   <div className="absolute z-10 left-0 top-0 text-white font-bold bg-black bg-opacity-45 backdrop-blur-3xl text-xs rounded-br-lg px-2 py-1 shadow-2xl">
@@ -69,17 +69,29 @@ async function More() {
               POPULAR ANIME
             </p>
           </div>
-          <div className="hover:small-scroll mt-6 mb-4 grid grid-flow-col auto-cols-max max-w-screen grid-rows-1 gap-3 md:gap-6 overflow-x-auto h-36 md:h-56">
+          <div className="hover:small-scroll mt-6 mb-4 grid grid-flow-col auto-cols-max max-w-screen grid-rows-1 gap-3 md:gap-6 overflow-x-auto h-72">
             {n.map((x, index) => (
               <div
-                className="relative w-24 md:w-36 h-full flex items-stretch"
+                className="overflow-hidden flex flex-col flex-shrink-0 cursor-pointer sm:w-44 h-72 w-36 hide-scroll relative rounded-md"
                 key={x?.title?.userPreferred}
               >
-                <img
-                  src={x?.coverImage?.large}
-                  alt="Anime pic"
-                  className="w-full h-full object-over opacity-75 border-2 border-violet-500"
-                />
+                <div className="absolute z-10 left-0 top-0 text-white font-bold bg-black bg-opacity-45 backdrop-blur-3xl text-xs rounded-br-lg px-2 py-1 shadow-2xl">
+                  {x?.genres ? x?.genres[0] : "Unknown"}
+                </div>
+                <div className="relative sm:w-44 sm:h-56 h-48 w-36">
+                  <img
+                    src={x?.coverImage?.large}
+                    alt="Anime pic"
+                    className="w-full h-full object-over opacity-90"
+                  />
+                  <div className="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-b from-transparent to-black z-10"></div>
+                </div>
+                <p className="p-1 text-xs md:text-sm text-white text-center overflow-hidden truncate">
+                  {x?.title?.userPreferred}
+                </p>
+                <p className="p-1 text-xs md:text-sm text-white opacity-85 text-center overflow-hidden">
+                  {x?.startDate?.year}
+                </p>
               </div>
             ))}
           </div>
