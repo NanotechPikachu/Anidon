927dff332b25e53d22a30e82b2f04c3106c7d3dd
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-04-18 09:14:46 +0530

    Update Refresh.jsx

--- app/functions/Refresh.jsx
+++ app/functions/Refresh.jsx
@@ -5,8 +5,8 @@ import { refresh } from './refresh-action.js';
 
 export function Refresh() {
   useEffect(() => {
-/*
     refresh(); console.log("+");
+/*
     const interval = setInterval(() => { refresh(); console.log("+") }, 60 * 1000);
     return () => clearInterval(interval);
 */
