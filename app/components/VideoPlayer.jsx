3ccafc737a0f71a540d45e4465bc2d05e982b37a
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-11-21 19:31:14 +0530

    Update VideoPlayer.jsx

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -1,32 +1,9 @@
-'use client'
-
-import React, { useRef, useEffect } from 'react';
-import Hls from 'hls.js';
+import Player from 'next-video/player';
 
 function VideoPlayer({ src }) {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    const hls = new Hls();
-    hls.loadSource(src);
-    hls.attachMedia(videoRef.current);
-    hls.on(Hls.Events.MANIFEST_LOADED, function () {
-      console.log('Manifest loaded.');
-    });
-    hls.on(Hls.Events.ERROR, function (event, data) {
-      if (data.fatal) {
-        console.error('Fatal error:', data.err);
-        hls.destroy();
-      }
-    });
-
-    return () => {
-      hls.destroy();
-    };
-  }, [src]);
 
   return (
-    <video ref={videoRef} width={640} height={360} controls />
+    <Player src={src} width={640} height={360} controls />
   );
 }
 
