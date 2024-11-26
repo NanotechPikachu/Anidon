729f377acd4d76e9f5080abb2c25d24fd7568e6c
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-11-26 14:02:41 +0530

    Update VideoPlayer.jsx

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -2,7 +2,7 @@
 
 function VideoPlayer({ src }) {
   return (
-    <video src={src} width={640} height={360} type="application/x-mpegURL" controls />
+    <video src={src} width={500} height={400} type="application/x-mpegURL" controls />
   );
 }
 
