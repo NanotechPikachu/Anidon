8acea7a2b82ab2f87add7e666ebbe2adc4286ea4
NanotechPikachu <tharunthonur@gmail.com>
2025-05-10 16:45:48 +0530

    Fix of mobile navbar not working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -52,7 +52,7 @@ export default function NavBar() {
       </div>
       {isOpen ? (
         <div className="w-full h-30 bg-blue-900 transition ease-in-out transition-transform translate-y-3 duration-300 lg:hidden">
-          <form className="ml-6 mr-6 p-16">
+          <form className="ml-6 mr-6 p-16" onSubmit={(e) => handleSubmit(e)}>
             <input
               type="text"
               className="bg-gray-200 text-gray-900 rounded-lg p-2 w-full mr-2"
