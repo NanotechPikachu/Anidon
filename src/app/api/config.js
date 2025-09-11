e96117d5d8c1d8c43b6407649fb6add5929be6a3
NanotechPikachu <tharunthonur@gmail.com>
2025-09-11 05:53:03 +0530

    fix: 500 error due to change in AP url and update next

--- src/app/api/config.js
+++ src/app/api/config.js
@@ -1,4 +1,4 @@
-const animepaheUrl = "https://animepahe.ru";
+const animepaheUrl = "https://animepahe.si";
 const USER_AGENT =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
 const headers = {
